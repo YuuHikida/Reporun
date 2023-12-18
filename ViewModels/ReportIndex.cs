547623b3c2cd8681702908dd202baf494d95ab58
YuuHikida <ponponda0103@gmail.com>
2023-12-18 22:06:11 +0900

    一旦

--- ViewModels/ReportIndex.cs
+++ ViewModels/ReportIndex.cs
@@ -13,5 +13,7 @@ namespace ReportSystem.ViewModels
         public List<Attendance> Attendances { get; set;}
 
         public List<Feedback>? Feedbacks {  get; set;} 
+
+        public bool LateJudgment { get; set; }
     }
 }
